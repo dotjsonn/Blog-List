ac0cfcc278f29a6ca3967e1a751dc34373342dc1
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-07-20 18:06:32 +0800

    feat: add login controller that check if the input is valid user and password before creating token

---
+++ controllers/login.js
@@ -0,0 +1,32 @@
+const loginRouter = require('express').Router()
+const bcrypt = require('bcryptjs')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+loginRouter.post('/', async (req, res) => {
+  const {username, password} = req.body
+
+  const user = await User.findOne({ username })
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.passwordHash)
+
+  if (!(user && passwordCorrect)) {
+    return res.status(401).send({
+      error: 'invalid username or password'
+    })
+  }
+
+  const tokenForUser = {
+    username: user.username,
+    id: user._id
+  }
+  const token = jwt.sign(tokenForUser, process.env.JWT_SECRET)
+  res.send({
+    token,
+    username: user.username,
+    name: user.name
+  })
+})
+
+module.exports = loginRouter
