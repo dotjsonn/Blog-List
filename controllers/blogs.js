df9f40429c4aa21b117ad5d3c5ac09d819810dc1
jsnspllrd@gmail.com <220439987+Shifardd@users.noreply.github.com>
2026-07-20 18:05:31 +0800

    feat: populate and add userExtractor to get user from middleware

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,40 +1,94 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const { userExtractor } = require('../utils/middleware')
 
 blogsRouter.get('/', async (req, res) => {
-  const blogs = await Blog.find({})
-  res.status(200).json(blogs)
+  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1, id: 1})
+  res.json(blogs)
 })
 
-blogsRouter.post('/', async (req, res) => {
-  const body = req.body
+blogsRouter.get('/:id', async (req, res) => {
+  const blog = await Blog.findById(req.params.id)
+  if(!blog) {
+    return res.status(404).end()
+  }
+  res.json(blog)
+})
 
-  if (!body.title || !body.url) {
-    return res.status(400).end()
+blogsRouter.post('/', userExtractor, async (req, res) => {
+  const blog = req.body
+  if(!blog.title || !blog.url) {
+    return res.status(400).send({error: 'title or url is missing'})
+  }
+  if(!blog.likes) {
+    blog.likes = 0
   }
 
-  const newBlog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes || 0
+  const user = req.user
+  
+  if(!user) {
+    return res.status(400).json({error: 'user missing'})
+  }
+
+  const blogObject = new Blog({
+    title: blog.title,
+    author: blog.author,
+    url: blog.url,
+    likes: blog.likes,
+    user: user._id.toString()
   })
 
-  const savedBlog = await newBlog.save()
-  res.status(201).json(newBlog)
+  const blogSaved = await blogObject.save()
+  user.blogs = user.blogs.concat(blogSaved._id)
+  await user.save()
+  res.status(201).json(blogSaved)
 })
 
-blogsRouter.delete('/:id', async (req, res) => {
-  const blogToBeDeleted = await Blog.findByIdAndDelete(req.params.id)
+blogsRouter.delete('/:id', userExtractor, async (req, res) => {
+  const blog = await Blog.findById(req.params.id)
+  const user = req.user
+  console.log(user);
+  console.log(blog);
+  
+  
+  const userid = user._id
+  if(blog.user.toString() === userid.toString()) {
+    await Blog.findByIdAndDelete(req.params.id)
+    await User.findByIdAndUpdate(userid, {
+      $pull: { blogs: req.params.id }
+    })
   res.status(204).end()
+  } else {
+    return res.status(401).send({ error: 'invalid token' })
+  }
+  
 })
 
-blogsRouter.put('/:id', async (req, res) => {
-  const { likes } = req.body
-  const blogToBeUpdated = await Blog.findById(req.params.id)
-  blogToBeUpdated.likes = likes
-  await blogToBeUpdated.save()
-  res.status(200).json(blogToBeUpdated)
+blogsRouter.put('/:id', userExtractor, async (req, res) => {
+  const { title, author, url, likes } = req.body
+
+  const blog = await Blog.findById(req.params.id)
+
+  if(!blog) {
+    return res.status(404).end()
+  }
+
+  const user = req.user
+  
+  if(!user) {
+    return res.status(400).json({error: 'user missing'})
+  }
+
+  blog.title = title
+  blog.author = author
+  blog.url = url
+  blog.likes = likes
+  blog.user = user._id.toString()
+
+  const updatedBlog = await blog.save()
+  res.json(updatedBlog)
 })
 
 module.exports = blogsRouter
